f0a9485b72db493d295fb0846564b42af597c749
Aleksey <dieselryder@mail.ru>
2022-10-19 12:55:41 +0300

    Task 15.5.1 fix

--- Module 15/task 15.5.1.go
+++ Module 15/task 15.5.1.go
@@ -17,7 +17,7 @@ func main() {
 	//var arr = [10]int{1, 1, 1, 2, 2, 2, 3, 3, 3, 4}
 	var arr [10]int
 	for i, _ := range arr {
-		fmt.Printf("Введите значение элементе массиве [%d]: ", i)
+		fmt.Printf("Введите значение элемента массива [%d]: ", i)
 		fmt.Scan(&arr[i])
 	}
 
