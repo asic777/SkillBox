ef6b61f957d3478ae2f92e0c2d609a38074e3d69
Aleksey <dieselryder@mail.ru>
2022-10-20 15:46:24 +0300

    Task 19.4.2

---
+++ Module19/task19.4.2.go
@@ -0,0 +1,25 @@
+// Задание 2. Сортировка пузырьком
+// Что нужно сделать
+// Отсортируйте массив длиной шесть пузырьком.
+
+package main
+
+import "fmt"
+
+func main() {
+	firstArray := [6]int{6, 5, 4, 3, 2, 1}
+	fmt.Println("Массив до сортировки:", firstArray)
+	fmt.Println("Массив после сортировки пузырьком:", sortArray(firstArray))
+
+}
+
+func sortArray(array [6]int) [6]int {
+	for i := 0; i < len(array); i++ {
+		for j := 1; j < len(array)-i; j++ {
+			if array[j-1] > array[j] {
+				array[j-1], array[j] = array[j], array[j-1]
+			}
+		}
+	}
+	return array
+}
